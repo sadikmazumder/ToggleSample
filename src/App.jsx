3dcaba90322c6e8795374da28a9c078f2d6fbb66
sadikmazumder <sadikmazumder@gmail.com>
2022-06-25 12:21:57 +0530

    Updated file extension

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,27 @@ function App() {
   const [show, setShow] = useState(false) 
 
 
-  function  getData(val){
+  function  OnInputChange(val){
 
     setData(val.target.value)
     //console.log(val.target.value)
   }
 
+  function onButtonClick(){
+    setShow(!show)
+  }
 
   return (
     <div className="App">
       
         <h1>{data}</h1>
-        <input type="text" onChange={getData} ></input><br/>  
+        <input type="text" onChange={OnInputChange} ></input><br/>  
         {
          show?<h1> Welcome to Website -{data}</h1> : null 
         }
        
         
-       <button onClick={()=>setShow(!show)}>{show ? 'Hide' : 'Show' }</button>  
+       <button onClick={onButtonClick}> {show ? 'Hide' : 'Show' } </button>  
       
     </div>
   )
